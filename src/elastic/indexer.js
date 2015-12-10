6952f27d24903b3d460255b291b9612908434ae1
Tomer <gmtomer@gmail.com>
2015-12-10 10:17:13 +0200

    started working on the indexer

---
+++ src/elastic/indexer.js
@@ -0,0 +1,7 @@
+import elastic from 'elasticsearch';
+import info from 'info.js';
+import Directory from '../utils/directory';
+
+export default function index(path) {
+    let directory = new Directory(path);
+}
