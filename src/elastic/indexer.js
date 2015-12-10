1c7f1596559c5f6aa7a9adee589a6978e80737bc
Tomer <gmtomer@gmail.com>
2015-12-10 10:24:34 +0200

    changed the indexer to class that receives elastic

--- src/elastic/indexer.js
+++ src/elastic/indexer.js
@@ -1,6 +1,12 @@
 import info from 'info.js';
 import Directory from '../utils/directory';
 
-export default function index(path) {
-    let directory = new Directory(path);
+export default class Indexer {
+    constructor (elasticClient) {
+        this.client = elasticClient;
+        this.index = this.index.bind(this);
+    }
+    index (path) {
+        let directory = new Directory(path);
+    }
 }
