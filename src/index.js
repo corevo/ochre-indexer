3cc65c3f6cffecdcbed3e902ffacd51754128c42
Tomer <gmtomer@gmail.com>
2015-12-10 10:55:19 +0200

    not sure how i want the index to look

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import elastic from 'elasticsearch';
+import Indexer from './elastic/indexer';
 
 let client = new elastic.Client({
     host: process.env.ELASTIC_SERVER || 'localhost:9200',
     sniffOnStart: true
 });
+let indexer = new Indexer(client);
+
+export default function indexDirectory(path) {
+    indexer.index(path);
+}
