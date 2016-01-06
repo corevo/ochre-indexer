28dba43502fd9392ccd39458d3a6fb64e5d13505
Tomer <gmtomer@gmail.com>
2016-01-06 18:55:57 +0200

    added body member for elastic document

--- src/elastic/indexer.js
+++ src/elastic/indexer.js
@@ -20,8 +20,10 @@ export default class Indexer {
                         index: 'files',
                         type: 'document',
                         id: md5(data),
-                        path: file.path,
-                        date: file.stats.ctime
+                        body: {
+                            path: file.path,
+                            date: file.stats.ctime
+                        }
                     };
                     info(file.path, (err, contents) => {
                         if (err) {
