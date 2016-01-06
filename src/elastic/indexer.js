cd9d8e8c16be3e859f82060665a1e40fef25fa1c
Tomer <gmtomer@gmail.com>
2016-01-06 19:37:50 +0200

    typos

--- src/elastic/indexer.js
+++ src/elastic/indexer.js
@@ -29,11 +29,11 @@ export default class Indexer {
                     };
                     info(file.path, (err, contents) => {
                         if (err) {
-                            console.error(err);
                         } else {
                             document.body.contents = contents;
                         }
                         let stats = getStats(file.path);
+                        console.log(stats);
                         if (stats)
                             document.body = Object.assign(document.body, stats);
                         this.client.index(document);
--- src/utils/file.js
+++ src/utils/file.js
@@ -5,7 +5,7 @@ export default function getFileStats(filePath, cb) {
     let filenameBegining = filePath.lastIndexOf(path.sep) + 1;
     let statsPath = `${filePath.substr(0, filenameBegining)}.${filePath.substr(filenameBegining)}.json`;
     try {
-        fs.accessSync(path, fs.R_OK);
+        fs.accessSync(statsPath, fs.R_OK);
         return require(statsPath);
     } catch (e) {
         return {};
