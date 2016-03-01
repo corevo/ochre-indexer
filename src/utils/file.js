ecc5f41918a9efd24afe740416912335433fc0c7
Tomer <gmtomer@gmail.com>
2016-03-01 14:57:56 +0200

    fixed a bug where tags file didnt exist

--- src/utils/file.js
+++ src/utils/file.js
@@ -4,5 +4,11 @@ import path from 'path';
 export default function getFileStats(filePath, cb) {
     let filenameBegining = filePath.lastIndexOf(path.sep) + 1;
     let statsPath = `${filePath.substr(0, filenameBegining)}.${filePath.substr(filenameBegining)}.json`;
-    return require(statsPath);
+    let stats = {};
+    try {
+        stats = require(statsPath);
+    } catch(err) {
+        console.log(`${filePath} doesn't have tags`);
+    }
+    return stats;
 }
