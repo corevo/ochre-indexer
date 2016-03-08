eb5c5202a7f0bacb4693adbd14ac5c2a60514d40
Tomer <gmtomer@gmail.com>
2016-03-08 15:27:36 +0200

    removed sniff on start

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import elastic from 'elasticsearch';
 import Indexer from './elastic/indexer';
 
 let client = new elastic.Client({
-    host: process.env.ELASTIC_SERVER || 'localhost:9200',
-    sniffOnStart: true
+    host: process.env.ELASTIC_SERVER || 'localhost:9200'
 });
 
 export default function indexDirectory(path, cb) {
