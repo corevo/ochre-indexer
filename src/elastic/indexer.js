a5ba1806c3b3c780927f5675998c40f39f59de63
Tomer <gmtomer@gmail.com>
2016-03-08 15:24:16 +0200

    refactor indexing to single files

--- src/elastic/indexer.js
+++ src/elastic/indexer.js
@@ -1,53 +1,39 @@
 import fs from 'fs';
+import path from 'path';
 import md5 from 'md5';
 import info from 'info.js';
 import Directory from '../utils/directory';
 
 export default class Indexer {
-    constructor (elasticClient, cb) {
+    constructor (elasticClient) {
         this.client = elasticClient;
         this.index = this.index.bind(this);
-        this.cb = cb;
     }
-    set amount (val) {
-        this._amount = val;
-        if (val === 0)
-            this.cb(this.directory.directories);
-    }
-    get amount () {
-        return this._amount;
-    }
-    index (path) {
-        this.directory = new Directory(path);
-        this.amount = this.directory.files.length;
-
-        this.directory.files.forEach(file => {
-            fs.readFile(file.path, (err, data) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    let document = {
-                        index: 'files',
-                        type: 'document',
-                        id: md5(data),
-                        body: {
-                            path: file.path,
-                            name: file.name,
-                            date: file.stats.ctime
-                        }
-                    };
-                    info(file.path, (err, contents) => {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            document.body.contents = contents;
-                        }
-                        this.client.index(document, () => {
-                            this.amount--;
-                        });
-                    });
-                }
-            })
-        });
+    index (file, cb) {
+        let stats = fs.statSync(file);
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                let document = {
+                    index: 'files',
+                    type: 'document',
+                    id: md5(data),
+                    body: {
+                        path: file,
+                        name: path.basename(file, path.extname(file)),
+                        date: stats.ctime
+                    }
+                };
+                info(file, (err, contents) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        document.body.contents = contents;
+                    }
+                    this.client.index(document, cb);
+                });
+            }
+        })
     }
 }
