8e8a876c279f939b459fe48754a09a5b9d26f077
Tomer <gmtomer@gmail.com>
2016-06-21 16:05:05 +0300

    so i can use it

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import elastic from 'elasticsearch';
 import Indexer from './elastic/indexer';
-
+import deleteByQuery from 'elasticsearch-deletebyquery';
 let client = new elastic.Client({
-    host: process.env.ELASTIC_SERVER || 'localhost:9200'
+    host: process.env.ELASTIC_SERVER || 'localhost:9200',
+    plugins: [ deleteByQuery ]
 });
 
 let indexer = new Indexer(client);
